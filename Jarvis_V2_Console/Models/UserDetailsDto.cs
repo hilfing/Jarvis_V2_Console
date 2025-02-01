183e1e511fb715ed17a24c66daeb5f9a31b884a8
HilFing <81067543+hilfing@users.noreply.github.com>
2025-02-02 00:15:43 +0530

    🔧 fix(Models/UserDetailsDto): remove default values for properties in UserDetailsDto class

--- Jarvis_V2_Console/Models/UserDetailsDto.cs
+++ Jarvis_V2_Console/Models/UserDetailsDto.cs
@@ -3,10 +3,10 @@ namespace Jarvis_V2_Console.Models;
 public class UserDetailsDto
 {
     public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string? Role { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
     public DateTime? LastLogin { get; set; }
 }
