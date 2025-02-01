0d75cb2a8dbc83dfdfad98ef10af4d5686b42981
HilFing <81067543+hilfing@users.noreply.github.com>
2025-02-02 00:16:52 +0530

    ✨ feat(Handlers/ChatAPIHandler): add ChatAPIHandler to generate JSON string for chat messages and history

---
+++ Jarvis_V2_Console/Handlers/ChatAPIHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Jarvis_V2_Console.Handlers;
+
+public class ChatAPIHandler
+{
+    // Method to generate the JSON string
+    public object GenerateChatJson(string mainMsg, List<Dictionary<string, string>> history)
+    {
+        // Create JSON object
+        var jsonObject = new 
+        {
+            msg = mainMsg,
+            history = history
+        };
+        
+        // Return the JSON string
+        return jsonObject;
+    }
+}
